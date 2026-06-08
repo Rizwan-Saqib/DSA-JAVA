c4e2a12c456e766d929e66aebffd3562f457879a
Muhammad Rizwan Saqib <rizwansaqib461@gmail.com>
2026-06-08 06:13:35 +0500

    Update DoublyLinkedList.java

--- LinkedList/doubly/DoublyLinkedList.java
+++ LinkedList/doubly/DoublyLinkedList.java
@@ -1,1 +1,37 @@
 
+public class DLL {
+    Node head;
+    Node last;
+
+    class Node {
+        int val;
+        Node prev;
+        Node next;
+
+        public Node(int val) {
+
+            this.val = val;
+            this.prev = null;
+            this.next = null;
+
+        }
+    }
+
+    public void addFirst(int val) {
+        Node newNode = new Node(val);
+
+        if (head == null) {
+            head = newNode;
+            last = newNode;
+            return;
+
+        }
+
+        newNode.prev = null;
+        newNode.next = head;
+
+        head.prev = newNode;
+
+        head = newNode;
+
+    }
